de655db476b326eb3b422d181c252a09c12210fc
zhamby <zhamby1@ivytech.edu>
2024-04-22 10:57:27 -0400

    added new allowed host

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+
+
+#this adds a post list function the defers to the post list html page. We respond to a request, send the browser to the associated html page, and pass any date on to it
+def post_list(request):
+    #this is where you are going to put your queries (for the most part)
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
